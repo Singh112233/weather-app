6034b313dd528a8ddc15f92c3c7065aec5e4e5b2
Singh112233 <78741049+Singh112233@users.noreply.github.com>
2024-12-05 13:46:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,124 +1,37 @@
-import React from "react";
-import "./App.css";
-import Form from "./app_component/form.component";
-import Weather from "./app_component/weather.component";
-import "bootstrap/dist/css/bootstrap.min.css";
+package com.springboot.azureapp.config;
 
-// git project https://github.com/erikflowers/weather-icons
-import "weather-icons/css/weather-icons.css";
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.azure.core.credential.TokenRequestContext;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-const Api_Key = "429736441cf3572838aa10530929f7cd";
+import javax.sql.DataSource;
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      city: undefined,
-      country: undefined,
-      icon: undefined,
-      main: undefined,
-      celsius: undefined,
-      temp_max: null,
-      temp_min: null,
-      description: "",
-      error: false
-    };
+@Configuration
+public class DataSourceConfig {
 
-    this.weatherIcon = {
-      Thunderstorm: "wi-thunderstorm",
-      Drizzle: "wi-sleet",
-      Rain: "wi-storm-showers",
-      Snow: "wi-snow",
-      Atmosphere: "wi-fog",
-      Clear: "wi-day-sunny",
-      Clouds: "wi-day-fog"
-    };
-  }
+    @Bean
+    public DataSource dataSource() {
+        // Build the Azure service principal credentials
+        ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
+                .clientId("f247fd99-ae63-4fb9-9406-d090f5427597")
+                
+                .tenantId("3a2c359c-2641-4abd-8bec-6a7fd85a1993")
+                .build();
 
-  get_WeatherIcon(icons, rangeId) {
-    switch (true) {
-      case rangeId >= 200 && rangeId < 232:
-        this.setState({ icon: icons.Thunderstorm });
-        break;
-      case rangeId >= 300 && rangeId <= 321:
-        this.setState({ icon: icons.Drizzle });
-        break;
-      case rangeId >= 500 && rangeId <= 521:
-        this.setState({ icon: icons.Rain });
-        break;
-      case rangeId >= 600 && rangeId <= 622:
-        this.setState({ icon: icons.Snow });
-        break;
-      case rangeId >= 701 && rangeId <= 781:
-        this.setState({ icon: icons.Atmosphere });
-        break;
-      case rangeId === 800:
-        this.setState({ icon: icons.Clear });
-        break;
-      case rangeId >= 801 && rangeId <= 804:
-        this.setState({ icon: icons.Clouds });
-        break;
-      default:
-        this.setState({ icon: icons.Clouds });
+
+        TokenRequestContext tokenRequestContext = new TokenRequestContext()
+                .addScopes("https://database.windows.net//.default");
+
+        // Retrieve the access token
+        String accessToken = clientSecretCredential.getToken(tokenRequestContext).block().getToken();
+
+        // Configure the DataSource with the access token
+        SQLServerDataSource dataSource = new SQLServerDataSource();
+        dataSource.setURL("jdbc:sqlserver://ashdbserver1.database.windows.net:1433;database=ashDB;encrypt=true;");
+        dataSource.setAccessToken(accessToken);
+        return dataSource;
     }
-  }
-
-  calCelsius(temp) {
-    let cell = Math.floor(temp - 273.15);
-    return cell;
-  }
-
-  getWeather = async e => {
-    e.preventDefault();
-
-    const country = e.target.elements.country.value;
-    const city = e.target.elements.city.value;
-
-    if (country && city) {
-      const api_call = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
-      );
-
-      const response = await api_call.json();
-
-      this.setState({
-        city: `${response.name}, ${response.sys.country}`,
-        country: response.sys.country,
-        main: response.weather[0].main,
-        celsius: this.calCelsius(response.main.temp),
-        temp_max: this.calCelsius(response.main.temp_max),
-        temp_min: this.calCelsius(response.main.temp_min),
-        description: response.weather[0].description,
-        error: false
-      });
-
-      // seting icons
-      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
-
-      console.log(response);
-    } else {
-      this.setState({
-        error: true
-      });
-    }
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <Form loadweather={this.getWeather} error={this.state.error} />
-        <Weather
-          cityname={this.state.city}
-          weatherIcon={this.state.icon}
-          temp_celsius={this.state.celsius}
-          temp_max={this.state.temp_max}
-          temp_min={this.state.temp_min}
-          description={this.state.description}
-        />
-      </div>
-    );
-  }
 }
-
-export default App;
-
